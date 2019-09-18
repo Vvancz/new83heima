245a356350e46af1fe13c601e9651c5dcef27e20
Vvancz <951745827@qq.com>
2019-09-18 09:45:11 +0800

    清理目录 导入样式

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui'
+import './styles/index.less'
+import 'element-ui/lib/theme-chalk/index.css'
+// Vue.config.productionTip = false
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
