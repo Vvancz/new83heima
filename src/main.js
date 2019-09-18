d2aa3d87ae717ec287271216788d56f0eb779661
Vvancz <951745827@qq.com>
2019-09-18 15:03:52 +0800

    配置axios和baseurl

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// import axios from '../node_modules/axios/dist/axios'
+import axios from 'axios'
+
 import ElementUI from 'element-ui'
 import './styles/index.less'
 import 'element-ui/lib/theme-chalk/index.css'
 // Vue.config.productionTip = false
 Vue.use(ElementUI)
+axios.defaults.baseURL = ' http://ttapi.research.itcast.cn/mp/'
+Vue.prototype.$axios = axios// 将axios共享给所有实例
 
 Vue.config.productionTip = false
 
