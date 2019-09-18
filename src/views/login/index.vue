72d7d5b347f854ba15486816254601dea10b7ff3
Vvancz <951745827@qq.com>
2019-09-18 10:53:15 +0800

    页面布局及样式

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,7 +1,33 @@
 <template>
   <div class="login">
     <!--  -->
-    <el-card class="login-card"></el-card>
+    <el-card class="login-card">
+      <div class="title">
+        <img src="../../assets/imgs/logo_index.png" alt />
+      </div>
+      <el-form style="margin-top:20px">
+        <el-form-item>
+          <!-- 手机号 -->
+          <el-input></el-input>
+        </el-form-item>
+
+        <el-form-item>
+          <!-- 验证码 -->
+          <el-input style="width:65%"></el-input>
+          <el-button style="float:right">发送验证码</el-button>
+        </el-form-item>
+
+        <el-form-item>
+            <!-- 同意选项 -->
+          <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+
+        <el-form-item>
+            <!-- 登录按钮 -->
+          <el-button type="primary" style="width:100%">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
   </div>
 </template>
 
@@ -16,13 +42,20 @@ export default {}
   height: 100vh;
   background-size: cover;
 
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-  .login-card{
-      width: 440px;
-      height: 330px;
+  .login-card {
+    width: 440px;
+    height: 340px;
+
+    .title {
+      text-align: center;
+      img {
+        height: 45px;
+      }
+    }
   }
 }
 </style>
