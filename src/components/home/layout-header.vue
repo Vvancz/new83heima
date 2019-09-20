c50aeb74c3939bd1cbf6cb10e88a517f2c797631
Vvancz <951745827@qq.com>
2019-09-20 10:58:00 +0800

    黑马头条PC-主页模块-头部信息查询

--- src/components/home/layout-header.vue
+++ src/components/home/layout-header.vue
@@ -12,12 +12,14 @@
     <!-- 右侧 -->
     <el-col :span="4">
         <!-- 用户头像 -->
-      <img class="head-img" src="../../assets/imgs/avatar.jpg" alt />
+      <img class="head-img" :src="userInfo.photo?userInfo.phpto:defaultImg" alt />
+      <!-- <img class="head-img" src="../../assets/imgs/avatar.jpg" alt /> -->
+
       <!-- 下拉菜单组件  -->
       <el-dropdown trigger="click">
         <!-- 匿名插槽 -->
         <span class="el-dropdown-link">
-          鹏鹏
+          {{userInfo.name}}
           <i class="el-icon-arrow-down el-icon--right"></i>
         </span>
         <!-- 具名插槽 -->
@@ -32,7 +34,30 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      userInfo: {
+        defaultImg: require('../../assets/imgs/avatar.jpg')// 转成base64
+      }
+    }
+  },
+  methods: {
+    // 获取用户个人资料
+    getUserInfo () {
+      let token = window.localStorage.getItem('user-token')
+      this.$axios({
+        url: '/user/profile',
+        headers: { 'Authorization': `Bearer${token}` }
+      }).then(result => {
+        this.userInfo = result.data.data
+      })
+    }
+  },
+  created () {
+    this.getUserInfo()
+  }
+}
 </script>
 
 <style lang="less" scoped>
