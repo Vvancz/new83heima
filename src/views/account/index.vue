e7f7f116b181a49e3618818e8f50551d6baa4135
Vvancz <951745827@qq.com>
2019-09-24 16:54:35 +0800

    账户信息 搭建页面

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -5,6 +5,30 @@
       <template slot="title">账户信息</template>
     </bread-crumb>
 
+    <!-- 表单 -->
+    <el-form style="margin-left:60px" label-width="100px">
+        <el-form-item label="用户名" >
+            <el-input style="width:300px"></el-input>
+        </el-form-item>
+        <el-form-item label="简介">
+            <el-input style="width:300px"></el-input>
+
+        </el-form-item>
+        <el-form-item label="邮箱">
+            <el-input style="width:300px"></el-input>
+
+        </el-form-item>
+        <el-form-item label="手机号">
+            <el-input style="width:300px"></el-input>
+
+        </el-form-item>
+        <el-form-item>
+            <el-button type="primary">保存信息</el-button>
+        </el-form-item>
+
+    </el-form>
+    <img class="head-img" src="../../assets/imgs/homea.jpg" alt="">
+
   </el-card>
 </template>
 
@@ -12,5 +36,13 @@
 export default {}
 </script>
 
-<style>
+<style lang='less' scoped>
+.head-img{
+    border-radius: 50%;
+    width: 200px;
+    height: 200px;
+    position: absolute;
+    top:150px;
+    right: 400px;
+}
 </style>
